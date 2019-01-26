af90004b5c2269527db0409e67420f087d10270a
Seth Miller <seth@four43.com>
2019-01-26 13:40:13 -0600

    WIP - Updating urxvt config for easy copy/paste and resizing

--- urxvt/install-urxvt-ubuntu.sh
+++ urxvt/install-urxvt-ubuntu.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt install rxvt-unicode
+# Just use the provided package
+sudo apt install -y rxvt-unicode
+
+EXT_DIR="$HOME/.urxvt/ext/"
+mkdir -p "${EXT_DIR}"
+cd "${EXT_DIR}"
+wget https://raw.githubusercontent.com/simmel/urxvt-resize-font/master/resize-font
