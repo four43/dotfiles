81aab2c17a00af0c4f2065e54c12b1005f58a26d
Seth Miller <seth@four43.com>
2019-02-04 21:55:52 -0600

    WIP - Polybar and theme settings

--- themes/mountain_lake_dark/init.d/50-polybar.sh
+++ themes/mountain_lake_dark/init.d/50-polybar.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 set -ex
-echo "Starting polybar!"
 
+echo "Killing currently running polybar instances.."
+pkill polybar || true
+
+echo "Starting polybar!"
 # We have a top and bottom polybar in this theme
 # primary-top, primary-bottom then for any other monitors: secondary-top, secondary-bottom
 
 PRIMARY_DISPLAY=$(${DOTFILE_DIR}/bin/x11/primary-display)
-MONITOR="${PRIMARY_DISPLAY}" polybar --config="${DOTFILE_DIR}/themes/current/polybar/config" primary-top
-MONITOR="${PRIMARY_DISPLAY}" polybar --config="${DOTFILE_DIR}/themes/current/polybar/config" primary-bottom
+
+ACTIVE_INTERFACE=$(${DOTFILE_DIR}/bin/active-network-interface)
+
+export LAN_WIRED_DEVICE=$(echo "${ACTIVE_INTERFACE}" | grep '^e') && echo "${CURRENT_INTERFACE}" || echo ""
+export LAN_WIRELESS_DEVICE=$(echo "${ACTIVE_INTERFACE}" | grep '^w') && echo "${CURRENT_INTERFACE}" || echo ""
+MONITOR="${PRIMARY_DISPLAY}" polybar --config="${DOTFILE_DIR}/themes/current/polybar/config" primary-top &
+MONITOR="${PRIMARY_DISPLAY}" polybar --config="${DOTFILE_DIR}/themes/current/polybar/config" primary-bottom &
+
+DISPLAY_ARR=( $(echo "${DISPLAY_ORDER}"))
+for DISPLAY in "${DISPLAY_ARR[@]}"; do
+    if [[ $DISPLAY != $PRIMARY_DISPLAY ]]; then
+        MONITOR="${DISPLAY}" polybar --config="${DOTFILE_DIR}/themes/current/polybar/config" secondary-top &
+        # MONITOR="${DISPLAY}" polybar --config="${DOTFILE_DIR}/themes/current/polybar/config" secondary-bottom &
+    fi
+done
---
+++ zsh/config/rc/50-command-completion.zsh
@@ -0,0 +1,5 @@
+autoload -U compinit
+compinit -d ~/.cache/zcompdump
+zstyle ':completion:*:manuals'    separate-sections true
+zstyle ':completion:*:manuals.*'  insert-sections   true
+zstyle ':completion:*:man:*'      menu yes select
