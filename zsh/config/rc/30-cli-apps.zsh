19c9503ae3af54dd883ed901e2e5422bad3af4c1
Seth Miller <smiller@aerisweather.com>
2019-10-24 18:34:56 -0500

    zsh - Added cheat helper

--- zsh/config/rc/30-cli-apps.zsh
+++ zsh/config/rc/30-cli-apps.zsh
@@ -2,4 +2,17 @@
 
 pathmunge "$HOME/.local/bin"
 
+function join-by { local IFS="$1"; shift; echo "$*"; }
+
+function cheat() {
+    if [[ $(echo "python|go" | grep "$1") ]]; then
+        local lang="$1"
+        shift
+        local query="$(join-by "+" $@)"
+        curl -s "cheat.sh/${lang}/${query}"
+    else
+        curl -s "cheat.sh/${1}"
+    fi
+}
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
