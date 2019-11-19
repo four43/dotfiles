478daebf06d3ae0e265ba38828e2ef4e5b594cab
Seth Miller <smiller@aerisweather.com>
2019-11-19 16:29:32 -0600

    zsh - added git shortrev for consistency across git versions

--- zsh/config/rc/99-git.zsh
+++ zsh/config/rc/99-git.zsh
@@ -3,3 +3,6 @@ export PATH="$PATH:$DOTFILE_DIR/bin/git"
 # Install autocomplete for git-* programs
 zstyle ':completion:*:*:git:*' user-commands ${${(M)${(k)commands}:#git-*}/git-/}
 
+function git-shortrev {
+    git rev-parse --short HEAD
+}
