46b8d5301beef60f5b2c38ef9bf36df39596af11
Seth Miller <smiller@aerisweather.com>
2019-11-19 16:28:47 -0600

    added password function, zsh helpers cleanup

--- zsh/config/rc/99-sh-helpers.zsh
+++ zsh/config/rc/99-sh-helpers.zsh
@@ -3,3 +3,12 @@
 function ls-details() {
     find -mindepth 1 $1 | sort | while read f; do { du -h "$f"; md5sum "$f"; } | sed 'N;s/\n/ /'; done | gawk '{print $1, $3, $4}' OFS='\t'
 }
+
+function emoji() {
+    cat ~/dotfiles/emojis | sed -E 's/^:([^:]+):/\1 - /' | fzf | awk '{print $3}' | tr -d "\n" | xclip -sel clip
+}
+
+function password() {
+    local length="${1:-16}"
+    base64 /dev/urandom | head -c "${length}" | awk '{print $1}'
+}
