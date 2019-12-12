6d229dd009fc64c0da807d2a95ae6458632a118f
Seth Miller <seth@four43.com>
2019-12-12 14:34:24 -0600

    Added postman install script

---
+++ postman/install-postman-ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+work_dir="$HOME/opt/postman"
+mkdir -p "$work_dir"
+cd "$work_dir"
+
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+tar -xzf ./postman.tar.gz
+
+postman_bin="$HOME/opt/postman/Postman/app/Postman"
+desktop_file_path="$HOME/.local/share/applications"
+
+mkdir -p "$desktop_file_path"
+
+echo "[Desktop Entry]
+Version=1.0
+Name=Postman
+Comment=Postman Native App
+Exec=$postman_bin — %u
+Icon=$(dirname "$postman_bin")/resources/app/assets/icon.png
+Terminal=false
+StartupWMClass=postman
+Type=Application
+Categories=Network;
+MimeType=x-scheme-handler/tg;
+X-Desktop-File-Install-Version=0.22" > "${desktop_file_path}/Postman.desktop"
