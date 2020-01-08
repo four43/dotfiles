7b928f45787b8e4db541168b8f8f50b991bddeba
Seth Miller <smiller@aerisweather.com>
2020-01-08 13:42:29 -0600

    Added nosqlbooster install script

---
+++ nosql-booster/install-nosql-booster-ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+work_dir="$HOME/opt/nosql-booster"
+rm -rf "$work_dir" || true
+mkdir -p "$work_dir"
+cd "$work_dir"
+
+wget https://nosqlbooster.com/s3/download/releasesv5/nosqlbooster4mongo-5.2.8.AppImage -O nosql-booster.AppImage
+chmod +x nosql-booster.AppImage
+./nosql-booster.AppImage --appimage-extract
+cp squashfs-root/nosqlbooster4mongo.png ./nosql-booster.png
+rm -rf squashfs-root
+
+nosql_booster_bin="$HOME/opt/nosql-booster/nosql-booster.AppImage"
+desktop_file_path="$HOME/.local/share/applications"
+
+mkdir -p "$desktop_file_path"
+
+echo "[Desktop Entry]
+Version=1.0
+Name=NoSQL Booster
+Comment=NoSQL Booster
+Exec=$nosql_booster_bin
+Icon=$(dirname "$nosql_booster_bin")/nosql-booster.png
+Terminal=false
+StartupWMClass=nosqlbooster
+Type=Application
+Categories=Network;
+" > "${desktop_file_path}/NoSQLBooster.desktop"
