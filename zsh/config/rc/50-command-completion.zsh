a2c7fe71925d8853c543bf6711a7754837e28f3c
Seth Miller <smiller@aerisweather.com>
2020-01-08 13:42:02 -0600

    zsh - Added completion for poetry, we'll see how that shakes out

--- zsh/config/rc/50-command-completion.zsh
+++ zsh/config/rc/50-command-completion.zsh
@@ -3,3 +3,10 @@ compinit -d ~/.cache/zcompdump
 zstyle ':completion:*:manuals'    separate-sections true
 zstyle ':completion:*:manuals.*'  insert-sections   true
 zstyle ':completion:*:man:*'      menu yes select
+
+if whence -cp  poetry 2>&1 >/dev/null; then
+    mkdir -p ~/.zfunc
+    poetry completions zsh > ~/.zfunc/_poetry
+fi
+
+fpath+=~/.zfunc
