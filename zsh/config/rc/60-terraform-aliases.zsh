c53855b8273afe8d2f8ee6f21fd2b07be517a656
Seth Miller <smiller@aerisweather.com>
2020-02-19 12:46:09 -0600

    Add zsh helpers for terraform and docker

---
+++ zsh/config/rc/60-terraform-aliases.zsh
@@ -0,0 +1,9 @@
+alias tf="terraform"
+function tf-ws {
+    tf_env="$1"
+    if [[ -z "$tf_env" ]]; then
+        terraform workspace list | sed -E 's/^[ \*]+//' | fzf | xargs -n 1 terraform workspace select
+    else
+        terraform workspace select "$tf_env"  
+    fi
+}
