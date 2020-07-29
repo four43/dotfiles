7ccffc4d7fefaf9815f7852fcb09e8ce8973d52f
Seth Miller <seth@four43.com>
2020-07-29 12:50:28 -0500

    Add aws cli v2 command completion

--- zsh/config/rc/50-command-completion.zsh
+++ zsh/config/rc/50-command-completion.zsh
@@ -9,4 +9,10 @@ if whence -cp  poetry 2>&1 >/dev/null; then
     poetry completions zsh > ~/.zfunc/_poetry
 fi
 
+aws_completer_path="$(whence -cp aws_completer 2>&1)"
+if [[ $? == 0 ]]; then
+    autoload bashcompinit && bashcompinit
+    complete -C "$aws_completer_path" aws
+fi
+
 fpath+=~/.zfunc
