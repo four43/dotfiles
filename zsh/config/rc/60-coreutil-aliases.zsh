76abcf963494e844eea32a7a4ead3a6fa3f26824
Seth Miller <seth@four43.com>
2020-08-20 14:14:10 -0500

    Fix ls-sub-dirs alias to numerically sort

--- zsh/config/rc/60-coreutil-aliases.zsh
+++ zsh/config/rc/60-coreutil-aliases.zsh
@@ -1,4 +1,4 @@
 alias ls='ls --color=auto --group-directories-first'
 alias ll='ls -l --color=auto --group-directories-first'
 alias rm='rm -i'
-alias ls-sub-dirs='(for d in */ ; do     echo "$(find $d -maxdepth 1 -type d | wc -l) $d"; done) | sort -r'
+alias ls-sub-dirs='(for d in */ ; do     echo "$(find $d -maxdepth 1 -type d | wc -l) $d"; done) | sort -rn'
