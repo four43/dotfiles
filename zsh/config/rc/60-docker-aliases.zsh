c5820ff209b4a1f54a19f2712ffd1aaa44923c8a
Seth Miller <seth@four43.com>
2021-02-27 11:35:24 -0600

    Update devops container version

--- zsh/config/rc/60-docker-aliases.zsh
+++ zsh/config/rc/60-docker-aliases.zsh
@@ -1,2 +1,2 @@
-alias devops-container='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/app -v /var/run/docker.sock:/var/run/docker.sock -w /app aerisweather/cicd-basics:2.12.1 /bin/bash'
+alias devops-container='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/app -v /var/run/docker.sock:/var/run/docker.sock -w /app aerisweather/cicd-basics:3 /bin/bash'
 alias docker-exec="docker ps --format '{{.Names}}' | fzf | xargs -o -n 1 -I % docker exec -it % /bin/bash"
