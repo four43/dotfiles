effc077ec894d9498b7154242d7a6c9bf9590ac3
Seth Miller <seth@four43.com>
2021-08-28 15:48:54 -0500

    Add zip-size-analysis helper

--- zsh/config/rc/30-cli-apps.zsh
+++ zsh/config/rc/30-cli-apps.zsh
@@ -2,7 +2,11 @@
 
 pathmunge "$HOME/.local/bin"
 
-function join-by { local IFS="$1"; shift; echo "$*"; }
+function join-by() {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
 
 function cheat() {
     if [[ $(echo "python|go" | grep "$1") ]]; then
@@ -13,6 +17,21 @@ function cheat() {
     else
         curl -s "cheat.sh/${1}"
     fi
+}
+
+function zip-size-analysis() {
+    (
+        local zip_file="$1"
+        local depth="${2:-1}"
+        TMP_DIR="$(mktemp -d)"
+        trap 'rm -rf "$TMP_DIR"' EXIT
+        unzip "$1" -d "$TMP_DIR" >/dev/null 2>&1 || exit $?
+        cd "${TMP_DIR}"
+        du -h --max-depth="$depth" "./" | head -n -1 | sort -hr
+        echo '---'
+        du -hs "${TMP_DIR}"
+    )
+    du -hs "${1}"
 }
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
