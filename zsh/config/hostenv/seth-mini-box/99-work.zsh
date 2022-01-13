cd39ba3aa0eff43fca26b17737d9a79f38a015bc
Seth Miller <seth@four43.com>
2022-01-13 10:18:38 -0600

    Update work config for home box for vpn-up alias

--- zsh/config/hostenv/seth-mini-box/99-work.zsh
+++ zsh/config/hostenv/seth-mini-box/99-work.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-alias vpn-up="TMUX_WINDOW_PREV_NAME=\"\$(tmux display-message -p '#W')\"; tmux rename-window '旅AW-VPN'; sudo ~/projects/infra-mgmt/util/vpn/openvpn-connect ~/Documents/AerisWeather/smiller.conf; tmux rename-window \$TMUX_WINDOW_PREV_NAME"
+alias vpn-up="TMUX_WINDOW_PREV_NAME=\"\$(tmux display-message -p '#W')\"; tmux rename-window '旅AW-VPN'; sudo ~/projects/aerisweather/infra-mgmt/util/vpn/openvpn-connect ~/Documents/AerisWeather/smiller.conf; tmux rename-window \$TMUX_WINDOW_PREV_NAME"
 
