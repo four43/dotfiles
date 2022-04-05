aa8775966dcee662b75effdf6a7d497b68ae98d4
Seth Miller <seth@four43.com>
2022-04-05 07:28:34 -0500

    Move python baseenvs to use work devtools better

--- zsh/config/rc/80-python.zsh
+++ zsh/config/rc/80-python.zsh
@@ -2,15 +2,15 @@
 setopt nullglob
 
 # Conda is used for base python installs (manually activate, theirs is big and complicated)
+conda_env="aw-geo-v1"
 conda_startup_script=(/opt/*conda*/etc/profile.d/conda.sh)
 if [[ -f "$conda_startup_script" ]]; then
   . "$conda_startup_script"
-  CONDA_CHANGEPS1=false conda activate base
+  CONDA_CHANGEPS1=false conda activate "${conda_env}"
 fi
 
 # Pipenv shouldn't mess with our prompt
 export VIRTUAL_ENV_DISABLE_PROMPT="true"
 
-CONDA_ENV_BASE_DIR="$(conda env list | grep "base" | head -n 1 | awk '{print $3}')"
-export GDAL_DATA="$CONDA_ENV_BASE_DIR/share/gdal"
-export PROJ_LIB="$CONDA_ENV_BASE_DIR/share/proj"
+export GDAL_DATA="$CONDA_PREFIX/share/gdal"
+export PROJ_LIB="$CONDA_PREFIX/share/proj"
