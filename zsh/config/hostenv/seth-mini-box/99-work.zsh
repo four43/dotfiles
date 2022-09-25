59b87773413abf429eff05d9890b00f1c59d70d8
Seth Miller <seth@four43.com>
2022-09-25 15:05:53 -0500

    Move vpn alias

--- zsh/config/hostenv/seth-mini-box/99-work.zsh
+++ zsh/config/hostenv/seth-mini-box/99-work.zsh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-alias vpn-up="TMUX_WINDOW_PREV_NAME=\"\$(tmux display-message -p '#W')\"; tmux rename-window '旅AW-VPN'; sudo ~/projects/aerisweather/infra-mgmt/util/vpn/openvpn-connect ~/Documents/AerisWeather/smiller.conf; tmux rename-window \$TMUX_WINDOW_PREV_NAME"
 
--- zsh/config/rc/99-aw.zsh
+++ zsh/config/rc/99-aw.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/zsh
 
 export PATH="${PATH}:$HOME/projects/aerisweather/devtools/aws"
+alias vpn-up="TMUX_WINDOW_PREV_NAME=\"\$(tmux display-message -p '#W')\"; tmux rename-window '旅AW-VPN'; sudo ~/projects/aerisweather/infra-mgmt/util/vpn/openvpn-connect ~/Documents/AerisWeather/smiller.conf; tmux rename-window \$TMUX_WINDOW_PREV_NAME"
