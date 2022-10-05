0f85ad50c11342860a9b3fc04e32e4e57eded4dd
Seth Miller <smiller@aerisweather.com>
2022-10-05 08:51:34 -0500

    Move nvm to opt

--- zsh/config/rc/99-nvm.zsh
+++ zsh/config/rc/99-nvm.zsh
@@ -1,3 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/opt/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
