c159c12a62fb5e53f4e3be9672d18d208d0295af
Seth Miller <seth@four43.com>
2023-01-03 07:18:16 -0600

    Add `proxy` command for some easy SOCKS proxying

--- zsh/config/rc/60-coreutil-aliases.zsh
+++ zsh/config/rc/60-coreutil-aliases.zsh
@@ -2,3 +2,8 @@ alias ls='ls --color=auto --group-directories-first'
 alias ll='ls -l --color=auto --group-directories-first'
 alias rm='rm -i'
 alias ls-sub-dirs='(for d in */ ; do     echo "$(find $d -maxdepth 1 -type d | wc -l) $d"; done) | sort -rn'
+
+function proxy() {
+    echo "Proxying port 1337 to $1"
+    ssh -D 1337 -qCN "$@"
+}
