557bf5ff4df093d80854cc0ca3e2ccc989514bc6
Seth Miller <seth@four43.com>
2023-01-03 07:18:32 -0600

    Fix docker-run-shell alias

--- zsh/config/rc/60-docker-aliases.zsh
+++ zsh/config/rc/60-docker-aliases.zsh
@@ -1,2 +1,3 @@
 alias docker-exec="docker ps --format '{{.Names}}' | fzf | xargs -o -n 1 -I % docker exec -it % /bin/bash"
+alias docker-run-shell="docker images --format '{{.Repository}}:{{.Tag}}' | fzf | xargs -o -I % docker run --rm -it --entrypoint /bin/bash %"
 alias docker-compose-shell="docker compose config --services | fzf | xargs -o -I % docker compose run --entrypoint /bin/bash %"
