69f3ec914161280ca52131601f64f2ab2e4fff82
Seth Miller <seth@four43.com>
2023-01-20 16:18:55 -0600

    Docker buildkit is useful sometimes I guess

--- zsh/config/rc/60-docker-aliases.zsh
+++ zsh/config/rc/60-docker-aliases.zsh
@@ -1,7 +1,19 @@
 # BUILDX removes in between containers that makes it really difficult to debug failing builds
-export DOCKER_BUILDX=0
-export DOCKER_BUILDKIT=0
+
 
 alias docker-exec="docker ps --format '{{.Names}}' | fzf | xargs -o -n 1 -I % docker exec -it % /bin/bash"
 alias docker-run-shell="docker images --format '{{.Repository}}:{{.Tag}}' | fzf | xargs -o -I % docker run --rm -it --entrypoint /bin/bash %"
 alias docker-compose-shell="docker compose config --services | fzf | xargs -o -I % docker compose run --entrypoint /bin/bash %"
+
+function docker-debug () {
+    # Docker is annoying to debug failing containers because it keeps auto cleaning things up. Buildkit is useful
+    # because it can actually cache multi-stage builds however. Annoying.
+    mode="${1}"
+    if [[ "$mode" == "on" ]] || [[ "$mode" == "true" ]]; then
+        export DOCKER_BUILDX=0
+        export DOCKER_BUILDKIT=0
+    else
+        export DOCKER_BUILDX=1
+        export DOCKER_BUILDKIT=1
+    fi
+}
