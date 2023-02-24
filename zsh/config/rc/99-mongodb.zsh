51214251b4d465994d4a9015ee8364ac631688e7
Seth Miller <seth@four43.com>
2023-02-24 13:14:34 -0600

    Add aliases for mongo commands (docker based)

---
+++ zsh/config/rc/99-mongodb.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+MONGODB_VERSION="4"
+alias mongodump="docker run --rm -i --network host --entrypoint mongodump mongo:${MONGODB_VERSION}"
+alias mongorestore="docker run --rm -i --network host --entrypoint mongorestore mongo:${MONGODB_VERSION}"
+
+# Backup an entire database:
+# mongodump --host [host] --db [db] --archive | mongorestore --host localhost --archive
+# Collection, just add --collection
