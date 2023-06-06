32ffcbed999ead6158bd333e913b736221b6ce25
Seth Miller <seth@four43.com>
2023-06-06 13:29:12 -0500

    Add cdktf alias for docker

--- zsh/config/rc/60-terraform-aliases.zsh
+++ zsh/config/rc/60-terraform-aliases.zsh
@@ -4,6 +4,8 @@ function tf-ws {
     if [[ -z "$tf_env" ]]; then
         terraform workspace list | sed -E 's/^[ \*]+//' | fzf | xargs -n 1 terraform workspace select
     else
-        terraform workspace select "$tf_env"  
+        terraform workspace select "$tf_env"
     fi
 }
+
+alias cdktf="docker compose run cdktf"
