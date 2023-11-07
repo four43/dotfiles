30b434d1de6404086956e15c705361139b9906ab
Seth Miller <seth@four43.com>
2023-11-06 20:07:22 -0600

    Add mongodb export alias to docker

--- zsh/config/rc/99-mongodb.zsh
+++ zsh/config/rc/99-mongodb.zsh
@@ -2,6 +2,7 @@
 MONGODB_VERSION="4"
 alias mongodump="docker run --rm -i --network host --entrypoint mongodump mongo:${MONGODB_VERSION}"
 alias mongorestore="docker run --rm -i --network host --entrypoint mongorestore mongo:${MONGODB_VERSION}"
+alias mongoexport="docker run --rm -i --network host --entrypoint mongoexport mongo:${MONGODB_VERSION}"
 
 # Backup an entire database:
 # mongodump --host [host] --db [db] --archive | mongorestore --host localhost --archive
