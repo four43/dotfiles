49f6628bab5537672ae9677f18cf7c0834adf947
Seth Miller <seth@four43.com>
2024-07-06 19:59:21 -0500

    Update keyboard LEDs

--- bin/home/keyboard-leds.sh
+++ bin/home/keyboard-leds.sh
@@ -2,10 +2,13 @@
 
 g815-led -a d8dee9
 g815-led -k logo 81a1c1
+g815-led -g keys cccccc
 g815-led -g gkeys b48ead
 g815-led -g modifiers b48ead
 g815-led -g multimedia b48ead
 g815-led -g fkeys ebcb8b
 g815-led -k esc a3be8c
 g815-led -g numeric a3be8c
+g815-led -g indicators ffffff
+g815-led -g capslock b48ead
 g815-led -c
