d5a14f41b2e1afcd8b1f958ebdc27567316e846d
Seth Miller <seth@four43.com>
2025-04-10 08:52:29 -0500

    Add system update helper

--- zsh/config/rc/99-sh-helpers.zsh
+++ zsh/config/rc/99-sh-helpers.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/zsh
+alias system-update="sudo apt update && sudo apt upgrade -y && flatpak update -y && flatpak uninstall --unused -y"
 
 function ls-details() {
     find -mindepth 1 $1 | sort | while read f; do { du -h "$f"; md5sum "$f"; } | sed 'N;s/\n/ /'; done | gawk '{print $1, $3, $4}' OFS='\t'
