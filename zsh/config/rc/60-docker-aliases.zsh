99843cd10894bb8cbb2efc45b676b00d6fb80960
Seth Miller <seth@four43.com>
2025-07-25 08:41:47 -0500

    fix: Docker env settings cleanup

--- zsh/config/rc/60-docker-aliases.zsh
+++ zsh/config/rc/60-docker-aliases.zsh
@@ -1,12 +1,15 @@
-# BUILDX removes in between containers that makes it really difficult to debug failing builds
 export BUILDX_NO_DEFAULT_ATTESTATIONS=1 # via https://stackoverflow.com/questions/75131872/error-failed-to-solve-failed-commit-on-ref-unexpected-status-400-bad-reques#comment137924263_75317155
 export COMPOSE_BAKE=true
+
+pathmunge "$HOME/.dotfiles/docker/cli-plugins" after
 
 alias docker-compose="docker compose"
 alias docker-exec="docker ps --format '{{.Names}}' | fzf | xargs -o -n 1 -I % docker exec -it % /bin/bash"
 alias docker-run-shell="docker images --format '{{.Repository}}:{{.Tag}}' | fzf | xargs -o -I % docker run --rm -it --entrypoint /bin/bash %"
 alias docker-compose-shell="docker compose config --services | fzf | xargs -o -I % docker compose run --entrypoint /bin/bash %"
 
+
+# BUILDX removes in between containers that makes it really difficult to debug failing builds
 function docker-debug () {
     # Docker is annoying to debug failing containers because it keeps auto cleaning things up. Buildkit is useful
     # because it can actually cache multi-stage builds however. Annoying.
