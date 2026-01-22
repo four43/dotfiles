331363de80d286e75aeb2613391c709270eb5a95
Seth Miller <seth@four43.com>
2026-01-22 14:32:03 -0600

    feat(zsh): Add better binding for home/end keys

--- zsh/config/rc/01-settings.zsh
+++ zsh/config/rc/01-settings.zsh
@@ -2,3 +2,11 @@ setopt SH_WORD_SPLIT
 
 bindkey -v
 bindkey '^R' history-incremental-search-backward
+
+# Home and End keys
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
+bindkey '^[[1~' beginning-of-line
+bindkey '^[[4~' end-of-line
+bindkey '\eOH' beginning-of-line
+bindkey '\eOF' end-of-line
