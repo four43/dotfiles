2f7466a6955bffc976e4cde0502f55a9a55fc64c
Seth Miller <seth@four43.com>
2026-02-20 10:15:25 -0600

    fix: Keybindings

--- zsh/config/rc/01-settings.zsh
+++ zsh/config/rc/01-settings.zsh
@@ -10,3 +10,15 @@ bindkey '^[[1~' beginning-of-line
 bindkey '^[[4~' end-of-line
 bindkey '\eOH' beginning-of-line
 bindkey '\eOF' end-of-line
+bindkey '^[[3~' delete-char
+bindkey '^?' backward-delete-char
+
+# Ctrl+Left/Right for word navigation
+bindkey '^[[1;5D' backward-word
+bindkey '^[[1;5C' forward-word
+
+# Shift+Tab for reverse menu completion
+bindkey '^[[Z' reverse-menu-complete
+
+# Insert key
+bindkey '^[[2~' overwrite-mode
