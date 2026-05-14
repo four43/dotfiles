99b1fef25e27549eb87399975e1c124cfad3a062
Seth Miller <seth@four43.com>
2026-05-14 15:52:42 -0500

    feat: git ssh cleanup

--- zsh/config/rc/99-git.zsh
+++ zsh/config/rc/99-git.zsh
@@ -3,6 +3,8 @@ DIR="$(dirname "$(readlink -f "$0")")"
 # Add git helpers to path
 export PATH="$PATH:$DOTFILE_DIR/bin/git"
 
+export GIT_SSH_COMMAND="$DOTFILE_DIR/git/git-ssh-key-rotation.sh"
+
 # Install autocomplete for git-* programs
 zstyle ':completion:*:*:git:*' user-commands ${${(M)${(k)commands}:#git-*}/git-/}
 
